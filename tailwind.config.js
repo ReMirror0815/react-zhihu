53c22af085941af5dc3714b0bc7fd3503f462df6
Mirror <15770851754@163.com>
2022-04-06 14:11:57 +0800

    feat(init): init

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+  },
+  plugins: [
+    
+  ],
+  variants: {
+    extend: {
+      color: ['last'],
+    }
+  },
+}
